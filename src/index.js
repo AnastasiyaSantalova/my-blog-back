85475442d4d1b4b2d1a61fedc01b81536d7c23ce
Nastia <nastia@MacBook-Air-Iliia.local>
2022-06-15 11:44:50 +0400

    [003-comments] comments functionality added

--- src/index.js
+++ src/index.js
@@ -3,10 +3,12 @@ import bodyParser from "body-parser";
 
 const articlesInfo = {
     'lorem-ipsum' : {
-        upvotes: 0
+        upvotes: 0,
+        comments: []
     },
     'dolor-sit': {
-        upvotes: 0
+        upvotes: 0,
+        comments: []
     }
 };
 
@@ -19,6 +21,14 @@ app.post('/api/articles/:name/upvote', (req, res) => {
 
     articlesInfo[articleName].upvotes += 1;
     res.status(200).send(`${articleName} now has ${articlesInfo[articleName].upvotes} upvotes`)
+});
+
+app.post('/api/articles/:name/add-comment', (req, res) => {
+    const { userName, text } = req.body;
+    const articleName = req.params.name;
+
+    articlesInfo[articleName].comments.push({ userName, text });
+    res.status(200).send(articlesInfo[articleName]);
 })
 
 app.listen(8000, () => console.log('8000 works!'));
